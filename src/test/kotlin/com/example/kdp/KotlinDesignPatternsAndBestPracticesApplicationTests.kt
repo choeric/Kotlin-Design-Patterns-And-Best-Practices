2168f5906ab5e2dcac56b59e569e80a5d4950994
henry.cho <henry.cho@koreainvestment.com>
2022-10-17 13:23:14 +0900

    first commit

---
+++ src/test/kotlin/com/example/kdp/KotlinDesignPatternsAndBestPracticesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kdp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDesignPatternsAndBestPracticesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
